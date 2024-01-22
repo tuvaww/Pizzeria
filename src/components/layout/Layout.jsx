024f1448fe310e208ef796e0ce9f82ef17323064
tuvaww <tuvali.wahlin@icloud.com>
2024-01-22 22:57:00 +0100

    started on home page

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './header/Header';
-import { Footer } from './footer/Footer';
+//import { Footer } from './footer/Footer';
 import './layout.scss';
 export const Layout = () => {
     return (
         <main className='main'>
             <Header />
-            <Outlet />
-
-            <Footer />
+            <div className='outlet'>
+                <Outlet />
+            </div>
+            {/*             <Footer />
+             */}{' '}
         </main>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-
+import './home.scss';
+import Pizza from '../assets/images/pizza_half.png';
 export const Home = () => {
-    return <div>home</div>;
+    return (
+        <div className='homeContainer'>
+            <div className='header'>
+                <img className='headerImg' src={Pizza} alt='pizza' />
+            </div>
+        </div>
+    );
 };
